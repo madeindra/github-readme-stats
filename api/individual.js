dcce6cc3f5f60cb496b648c02a501bceffe0ec5f
Made Indra <made.indra@protonmail.com>
2020-11-07 12:37:05 +0700

    WIP: Individual status API

---
+++ api/individual.js
@@ -0,0 +1,55 @@
+require("dotenv").config();
+const {
+  renderError,
+  parseBoolean,
+  parseArray,
+  clampValue,
+  CONSTANTS,
+} = require("../src/utils");
+const fetchStats = require("../src/fetchStats");
+const renderIndividualValue = require("../src/renderIndividualValue");
+
+module.exports = async (req, res) => {
+  const {
+    username,
+    show,
+    count_private,
+    title_color,
+    text_color,
+    bg_color,
+    theme,
+    cache_seconds,
+  } = req.query;
+  let stats;
+
+  res.setHeader("Content-Type", "image/svg+xml");
+
+  try {
+    stats = await fetchStats(username, parseBoolean(count_private));
+  } catch (err) {
+    return res.send(
+      renderError(
+        err.message,
+        "Make sure the provided username is not an organization"
+      )
+    );
+  }
+
+  const cacheSeconds = clampValue(
+    parseInt(cache_seconds || CONSTANTS.THIRTY_MINUTES, 10),
+    CONSTANTS.THIRTY_MINUTES,
+    CONSTANTS.ONE_DAY
+  );
+
+  res.setHeader("Cache-Control", `public, max-age=${cacheSeconds}`);
+
+  res.send(
+    renderIndividualValue(stats, {
+      show: parseArray(show),
+      title_color,
+      text_color,
+      bg_color,
+      theme,
+    })
+  );
+};
